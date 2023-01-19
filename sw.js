6b1a1948d6c9748aeea3d26ba81b4087fe83b808
Nikolay2193 <123067156+Nikolay2193@users.noreply.github.com>
2023-01-19 14:32:20 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const staticCaheName = 'site-static-v1'
+const assets = [
+    ' . ',
+    'index.html',
+    'app.js',
+    'images/icon.png',
+    'css/style.css',
+    'images/icons/icon-128x128.png',
+    'images/icons/icon-192x192.png'
+]
+
+
+    self.addEventListener ('install', evt => {
+        evt.waitUntil (
+            caches.open(staticCaheName) .then((cache) => {
+                console.log('Кэширование ресурсов')
+                cache.addAll(assets)
+            })
+        )
+    })
+
+    
+    self.addEventListener('fetch', evt => {
+        evt.waitUntil(
+            caches.keys().then(keys => {
+                return Promise.all (keys
+                    .filter(key => key !==staticCaheName)
+                    .map(key => caches.delete(key))
+                );
+            })
+        )
+    })
+
+
+    self.addEventListener('fetch', evt => {
+
+        evt.respondWith(
+            caches.match(evt.request).then(cacheRes => {
+                return cacheRes || fetch(evt.request)
+            })
+        )
+    })
